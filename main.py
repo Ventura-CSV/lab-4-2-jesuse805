b2b2873f4e4c52b3724077915ef071e824c21339
jesus <jesus_estrada5@my.vcccd.edu>
2024-06-27 19:20:31 -0700

    CS V11

--- main.py
+++ main.py
@@ -4,17 +4,21 @@ def main():
     Code Your Program here
     ########################################
     """
+    
     total = 0
 
     for _ in range(5):
-        inputState = int(input('Input an integer value: '))
-        total += 
+        
+        inputLab4 = int(input('Input an integer value: '))
+        
+        total += inputLab4
+        
         
     ########################################
     # Do not delete the return statement
     ########################################
+    print(total)
+    
     return total
-
-
 if __name__ == '__main__':
     main()
